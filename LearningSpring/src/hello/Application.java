559f5d24d9a64175a91f6e02711d2edcaa5f9c5b
micheng-MSTR <ming.cheng.0830@gmail.com>
2014-08-15 18:19:23 +0800

    annotations

--- LearningSpring/src/hello/Application.java
+++ LearningSpring/src/hello/Application.java
@@ -3,11 +3,22 @@ package hello;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 
-@Configuration
+@Configuration 
+/* @Configuration
+ * indicates that a class declares one or more @Bean methods and may be processed by the Spring container to generate
+ * bean definitions and service requests for those beans at runtime
+ */
 @ComponentScan
+/*
+ * @ComponentScan
+ * Configures component scanning directives for use with @Configuration classes
+ * If specific packages are not defined scanning will occur from the package of the class with this annotation.
+ * In this case it will scan hello package.
+ */
 public class Application {
 
     @Bean
+    // instantiate, configure and return a bean
     MessageService mockMessageService() {
         return new MessageService() {
             public String getMessage() {
